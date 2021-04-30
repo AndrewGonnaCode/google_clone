3d33919c71e2d020c0d4ea326fe57f0a1f8fe61d
Andrew <WH.Andrewa@yandex.ru>
2021-04-30 15:43:00 +0300

    enviroment

--- keys.js
+++
@@ -1,4 +0,0 @@
-// API KEY - https://developers.google.com/custom-search/v1/using_rest
-export const API_KEY = 'AIzaSyDKA3JHu4Y9eOQOzBpbxx2gY5eErRbZuyg'
-// CONTEXT KEY - https://cse.google.com/cse/create/new
-export const CONTEXT_KEY = '481d371e85d69f0a8'
--- pages/search.jsx
+++ pages/search.jsx
@@ -3,12 +3,14 @@ import { useRouter } from 'next/router'
 import React from 'react'
 import Header from '../components/Header'
 import SearchResults from '../components/SearchResults'
-import { API_KEY, CONTEXT_KEY } from '../keys'
 import Response from '../response.json'
 
+
 const Search = ({ results }) => {
+
+
+
 	const router = useRouter()
-	console.log(results);
 	return (
 		<div>
 			<Head>
@@ -25,9 +27,10 @@ export default Search
 
 
 export async function getServerSideProps(context) {
+	// console.log('ENV', process.env.DB_API_KEY);
 	const useDummyData = false;
 	const startIndex = context.query.start || "0"
-	const data = useDummyData ? Response : await (await fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`)).json()
+	const data = useDummyData ? Response : await (await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.DB_API_KEY}&cx=${process.env.DB_CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`)).json()
 	return {
 		props: {
 			results: data
